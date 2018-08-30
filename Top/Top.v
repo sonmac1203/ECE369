49735ac180cdb46b697e442fb99eec8897314769
lightdz <mitch_dz@hotmail.com>
2018-08-30 11:35:22 -0700

    added Top

---
+++ Top/Top.v
@@ -0,0 +1,41 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 08/30/2018 10:51:30 AM
+// Design Name: 
+// Module Name: Top
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module Top(Clk, Reset, out7, en_out, Instruction);
+
+    input Clk, Reset;
+    output [7:0] en_out;
+    output [6:0] out7;
+    wire Clk_out;
+    output [31:0] Instruction;
+
+    //module ClkDiv(Clk, Rst, ClkOut);
+    ClkDiv IFU_Clk(Clk, Reset, Clk_out);
+    
+    //module InstructionFetchUnit(Reset, Clk, Instruction);
+    InstructionFetchUnit top_IFU(Reset, Clk_out, Instruction);
+    
+    //module Two4DigitDisplay(Clk, NumberA, NumberB, out7, en_out);
+    Two4DigitDisplay top_Display(Clk_out, Instruction[15:0], Instruction[31:16], out7, en_out);
+
+
+endmodule
---
+++ Top/Top_tb.v
@@ -0,0 +1,54 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 08/30/2018 11:08:19 AM
+// Design Name: 
+// Module Name: Top_tb
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module Top_tb();
+
+//module Top(Clk, Reset, out7, en_out);
+
+   reg Reset, Clk;
+   wire [6:0] out7;
+   wire [7:0] en_out;
+   wire [31:0] Instruction;
+    
+    
+   Top t1(
+       .Clk(Clk),
+       .Reset(Reset),
+       .out7(out7),
+       .en_out(en_out),
+       .Instruction(Instruction)
+   );
+    
+   initial begin
+       Clk <= 1'b0;
+       forever #100 Clk <= ~Clk;
+   end
+   
+   initial begin
+   #200;
+   Reset = 1;
+   
+   #200;
+   Reset = 0;
+   end
+   
+endmodule
