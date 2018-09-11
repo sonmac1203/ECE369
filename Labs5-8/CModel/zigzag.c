5431f8579b5618975239408fa0040849a35daed6
lightdz <mitch_dz@hotmail.com>
2018-09-10 17:34:35 -0700

    changed checkSAD < min to be checkSAD <= min

--- Labs5-8/CModel/zigzag.c
+++ Labs5-8/CModel/zigzag.c
@@ -62,30 +62,30 @@ int FrameLoc(int x, int y, int frameWidth){
 
 
 
-int main(int * frame, int * window, int * asize){
-	frame[] = {	0, 1, 2, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 
-         			1, 2, 3, 4, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 
-         			2, 3, 32, 1, 2, 3, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 
-         			3, 4, 1, 2, 3, 4, 18, 21, 24, 27, 30, 33, 36, 39, 42, 45, 
-         			0, 4, 2, 3, 4, 5, 24, 28, 32, 36, 40, 44, 48, 52, 56, 60, 
-         			0, 5, 3, 4, 5, 6, 30, 35, 40, 45, 50, 55, 60, 65, 70,  75, 
-         			0, 6, 12, 18, 24, 30, 36, 42, 48, 54, 60, 66, 72, 78, 84, 90, 
-         			0, 4, 14, 21, 28, 35, 42, 49, 56, 63, 70, 77, 84, 91, 98, 105, 
-         			0, 8, 16, 24, 32, 40, 48, 56, 64, 72, 80, 88, 96, 104, 112, 120, 
-         			0, 9, 18, 27, 36, 45, 54, 63, 72, 81, 90, 99, 108, 117, 126, 135, 
-         			0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150, 
-         			0, 11, 22, 33, 44, 55, 66, 77, 88, 99, 110, 121, 132, 143, 154, 165, 
-         			0, 12, 24, 36, 48, 60, 72, 84, 96, 108, 120, 132, 0, 1, 2, 3, 
-         			0, 13, 26, 39, 52, 65, 78, 91, 104, 114, 130, 143, 1, 2, 3, 4, 
-         			0, 14, 28, 42, 56, 70, 84, 98, 112, 126, 140, 154, 2, 3, 4, 5, 
-         			0, 15, 30, 45, 60, 75, 90, 105, 120, 135, 150, 165, 3, 4, 5, 6 };
+int main(int * asize, int * frame, int * window){
+	// frame[] = {	0, 1, 2, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 
+ //         			1, 2, 3, 4, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 
+ //         			2, 3, 32, 1, 2, 3, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 
+ //         			3, 4, 1, 2, 3, 4, 18, 21, 24, 27, 30, 33, 36, 39, 42, 45, 
+ //         			0, 4, 2, 3, 4, 5, 24, 28, 32, 36, 40, 44, 48, 52, 56, 60, 
+ //         			0, 5, 3, 4, 5, 6, 30, 35, 40, 45, 50, 55, 60, 65, 70,  75, 
+ //         			0, 6, 12, 18, 24, 30, 36, 42, 48, 54, 60, 66, 72, 78, 84, 90, 
+ //         			0, 4, 14, 21, 28, 35, 42, 49, 56, 63, 70, 77, 84, 91, 98, 105, 
+ //         			0, 8, 16, 24, 32, 40, 48, 56, 64, 72, 80, 88, 96, 104, 112, 120, 
+ //         			0, 9, 18, 27, 36, 45, 54, 63, 72, 81, 90, 99, 108, 117, 126, 135, 
+ //         			0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150, 
+ //         			0, 11, 22, 33, 44, 55, 66, 77, 88, 99, 110, 121, 132, 143, 154, 165, 
+ //         			0, 12, 24, 36, 48, 60, 72, 84, 96, 108, 120, 132, 0, 1, 2, 3, 
+ //         			0, 13, 26, 39, 52, 65, 78, 91, 104, 114, 130, 143, 1, 2, 3, 4, 
+ //         			0, 14, 28, 42, 56, 70, 84, 98, 112, 126, 140, 154, 2, 3, 4, 5, 
+ //         			0, 15, 30, 45, 60, 75, 90, 105, 120, 135, 150, 165, 3, 4, 5, 6 };
 
-    window[] = {0, 1, 2, 3, 
-         			1, 2, 3, 4, 
-         			2, 3, 4, 5, 
-         			3, 4, 5, 6 };
+ //    window[] = {0, 1, 2, 3, 
+ //         			1, 2, 3, 4, 
+ //         			2, 3, 4, 5, 
+ //         			3, 4, 5, 6 };
 
-    asize[] = {16, 16, 4, 4};
+ //    asize[] = {16, 16, 4, 4};
 
 
     int frameSizeX = asize[0];
@@ -107,7 +107,7 @@ int main(int * frame, int * window, int * asize){
 
     if(frameSizeX > windowSizeX && frameSizeY > windowSizeY){
         //int SAD(int * frame, int * window, int * asize, int frameX, int frameY){
-        if ((temp = SAD(frame, window, asize, frameLoc[0], frameLoc[1])) < min[0]){min[0] = temp;min[1] = frameLoc[0];min[2] = frameLoc[1];}
+        if ((temp = SAD(frame, window, asize, frameLoc[0], frameLoc[1])) <= min[0]){min[0] = temp;min[1] = frameLoc[0];min[2] = frameLoc[1];}
 
         //error detect right
         if (frameLoc[0] < frameSizeX - windowSizeX){
@@ -126,7 +126,7 @@ int main(int * frame, int * window, int * asize){
             }
         }
 
-        if ((temp = SAD(frame, window, asize, frameLoc[0], frameLoc[1])) < min[0]){min[0] = temp;min[1] = frameLoc[0];min[2] = frameLoc[1];}
+        if ((temp = SAD(frame, window, asize, frameLoc[0], frameLoc[1])) <= min[0]){min[0] = temp;min[1] = frameLoc[0];min[2] = frameLoc[1];}
 
 
 
@@ -157,10 +157,10 @@ int main(int * frame, int * window, int * asize){
                     loopflag = 0;
                 }
             
-                if ((temp = SAD(frame, window, asize, frameLoc[0], frameLoc[1])) < min[0]){min[0] = temp;min[1] = frameLoc[0];min[2] = frameLoc[1];}
+                if ((temp = SAD(frame, window, asize, frameLoc[0], frameLoc[1])) <= min[0]){min[0] = temp;min[1] = frameLoc[0];min[2] = frameLoc[1];}
             }
 
-            if ((temp = SAD(frame, window, asize, frameLoc[0], frameLoc[1])) < min[0]){min[0] = temp;min[1] = frameLoc[0];min[2] = frameLoc[1];}
+            if ((temp = SAD(frame, window, asize, frameLoc[0], frameLoc[1])) <= min[0]){min[0] = temp;min[1] = frameLoc[0];min[2] = frameLoc[1];}
             if(finalLocation == FrameLoc(frameLoc[0], frameLoc[1], frameSizeX))
                 break;
 
@@ -188,10 +188,10 @@ int main(int * frame, int * window, int * asize){
                     loopflag = 0;
                 }
             
-                if ((temp = SAD(frame, window, asize, frameLoc[0], frameLoc[1])) < min[0]){min[0] = temp;min[1] = frameLoc[0];min[2] = frameLoc[1];}
+                if ((temp = SAD(frame, window, asize, frameLoc[0], frameLoc[1])) <= min[0]){min[0] = temp;min[1] = frameLoc[0];min[2] = frameLoc[1];}
             }
 
-            if ((temp = SAD(frame, window, asize, frameLoc[0], frameLoc[1])) < min[0]){min[0] = temp;min[1] = frameLoc[0];min[2] = frameLoc[1];}
+            if ((temp = SAD(frame, window, asize, frameLoc[0], frameLoc[1])) <= min[0]){min[0] = temp;min[1] = frameLoc[0];min[2] = frameLoc[1];}
             if(finalLocation == FrameLoc(frameLoc[0], frameLoc[1], frameSizeX))
                 break;
         }
