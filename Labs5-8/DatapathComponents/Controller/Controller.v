b69fa9dae637dbf605f1af0970464e0e51dc9b0b
lightdz <mitch_dz@hotmail.com>
2018-10-12 11:03:22 -0700

    init Controller

---
+++ Labs5-8/DatapathComponents/Controller/Controller.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 10/12/2018 11:01:02 AM
+// Design Name: 
+// Module Name: Controller
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module Controller(
+
+    );
+endmodule
---
+++ Labs5-8/DatapathComponents/Controller/Controller_tb.v
@@ -0,0 +1,27 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 10/12/2018 11:01:50 AM
+// Design Name: 
+// Module Name: Controller_tb
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module Controller_tb();
+
+    Controller c0();
+
+endmodule
