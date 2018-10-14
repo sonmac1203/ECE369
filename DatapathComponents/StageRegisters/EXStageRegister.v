bee2787eae778bd2aadd3e750bb4a20c50647b6b
lightdz <mitch_dz@hotmail.com>
2018-10-14 12:41:16 -0700

    init stage registers

---
+++ DatapathComponents/StageRegisters/EXStageRegister.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 10/14/2018 12:35:25 PM
+// Design Name: 
+// Module Name: EXStageRegister
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module EXStageRegister(
+
+    );
+endmodule
---
+++ DatapathComponents/StageRegisters/WBStageRegister.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 10/14/2018 12:34:01 PM
+// Design Name: 
+// Module Name: WBStageRegister
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module WBStageRegister(
+
+    );
+endmodule
