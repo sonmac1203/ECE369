830a237d4682310622c361ada589d47dcd327b16
lightdz <mitch_dz@hotmail.com>
2018-10-14 22:02:03 -0700

    initial commit for ZeroExtension

---
+++ DatapathComponents/ZeroExtension.v
@@ -0,0 +1,40 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 10/14/2018 09:54:10 PM
+// Design Name: 
+// Module Name: ZeroExtension
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module ZeroExtension(in, out);
+
+    /* A 16-Bit input word */
+    input [15:0] in;
+    
+    /* A 32-Bit output word */
+    output reg [31:0] out;
+    
+    
+    always @ (*)    begin
+
+        out <= {16'b0, in};
+    
+    end
+    
+    
+
+endmodule
