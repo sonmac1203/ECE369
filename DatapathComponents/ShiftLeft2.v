b71571d8962f3c7ce60a0d209dc2ed643305e628
lightdz <mitch_dz@hotmail.com>
2018-10-17 15:32:56 -0700

    init ShiftLeft2.v

---
+++ DatapathComponents/ShiftLeft2.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 10/17/2018 03:32:29 PM
+// Design Name: 
+// Module Name: ShiftLeft2
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module ShiftLeft2(
+
+    );
+endmodule
