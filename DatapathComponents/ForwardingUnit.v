5448a6d9e9ab2c39423c49ebe7748c15cb352b46
lightdz <mitch_dz@hotmail.com>
2018-11-01 11:50:12 -0700

    init forwarding

---
+++ DatapathComponents/ForwardingUnit.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 11/01/2018 11:49:27 AM
+// Design Name: 
+// Module Name: ForwardingUnit
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module ForwardingUnit(
+
+    );
+endmodule
